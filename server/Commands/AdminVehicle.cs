2d0da76513dc892768fc3e3ad2cf8dcd4c763b33
FirstWiseman <blocklinlp@gmail.com>
2023-04-09 23:30:58 +0200

    Added dv with Radius

--- server/Commands/AdminVehicle.cs
+++ server/Commands/AdminVehicle.cs
@@ -13,16 +13,27 @@ internal class AdminVehicle : IScript
   internal static IVehicleHandler _vehicleHandler = new VehicleHandler();
 
   [Command("car")]
-  public static async void Execute(xPlayer player, string model)
-  {   
+  public static async void Car(xPlayer player, string model)
+  {
     Position pos = player.Position;
     Rotation rot = player.Rotation;
 
     xVehicle veh = await _vehicleHandler.CreateVehicle(model, pos, rot);
-    if(veh.Exists){
-      player.SendChatMessage("Spawned Vehicle: "+model);
+    if (veh.Exists)
+    {
+      player.SendChatMessage("Spawned Vehicle: " + model);
     }
   }
 
+  [Command("dv")]
+  public static void DeleteVehicle(xPlayer player, int range = 5)
+  {
+    Position pos = player.Position;
+    List<xVehicle> vehicles = _vehicleHandler.GetVehiclesInRadius(pos, range);
 
+    vehicles.ForEach(delegate (xVehicle veh)
+    {
+      veh.Destroy();
+    });
+  }
 }
