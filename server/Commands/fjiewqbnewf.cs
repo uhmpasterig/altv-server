b06ac661ecad12b3ac420ff668f324a1f539eb64
uhmpasterig <805luca805@gmail.com>
2023-04-09 23:03:22 +0200

    feat: create fjiewqbnewf.cs

---
+++ server/Commands/fjiewqbnewf.cs
@@ -0,0 +1,14 @@
+using System;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Resources.Chat.Api;
+
+namespace server.Commands;
+
+class fjiewqbnewf
+{
+  [Command("fjiewqbnewf")]
+  public static void Execute(IPlayer player, string[] args)
+  {
+    player.SendChatMessage("fjiewqbnewf");
+  }
+}
