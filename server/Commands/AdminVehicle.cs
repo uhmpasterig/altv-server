345191c496af3c9a819338f0ae696991461d27bc
FirstWiseman <blocklinlp@gmail.com>
2023-04-10 02:33:54 +0200

    update AdminVehicle.cs

--- server/Commands/AdminVehicle.cs
+++ server/Commands/AdminVehicle.cs
@@ -57,6 +57,7 @@ internal class AdminVehicle : IScript
 
     player.SendChatMessage("Vehicle Exists: "+veh.Exists);
     if(veh.Exists){
+    player.SendChatMessage("Set Fulltune");
       _vehicleHandler.SetModByType(veh, VehicleModType.Horns, 20);
       _vehicleHandler.SetModByType(veh, VehicleModType.Spoilers, 2);
       _vehicleHandler.SetModByType(veh, VehicleModType.Color1, 5);
