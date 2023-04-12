ed5a33423427caa95a0ae37b4a269201fe1c6794
uhmpasterig <805luca805@gmail.com>
2023-04-12 15:01:10 +0200

    update xPlayer.cs and PlayerLoadedWriter.cs

---
+++ Modules/Util/Config.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AltV.Net.Data;
+namespace server.Util.Player;
+
+public class Positions {
+  public static Position Spawn = new Position(0, 0, 0);
+  
+}
--- Modules/Util/Player/PlayerLoadedWriter.cs
+++ Modules/Util/Player/PlayerLoadedWriter.cs
@@ -5,48 +5,46 @@ using AltV.Net;
 using server.Core;
 using server.Models;
 
-namespace server.Util.Player
+namespace server.Util.Player;
+public class PlayerLoadedWriter : IWritable
 {
-    public class PlayerLoadedWriter : IWritable
+  private readonly xPlayer player;
+  public PlayerLoadedWriter(xPlayer _player)
+  {
+    this.player = _player;
+  }
+
+  public void OnWrite(IMValueWriter writer)
+  {
+    writer.BeginObject();
+    writer.Name("name");
+    writer.Value(player.name);
+    writer.Name("id");
+    writer.Value(player.id);
+    writer.Name("maxArmor");
+    writer.Value(player.MaxArmor);
+    writer.Name("maxHealth");
+    writer.Value(player.MaxHealth);
+    writer.Name("armor");
+    writer.Value(player.Armor);
+    writer.Name("health");
+    writer.Value(player.Health);
+
+
+    /* writer.BeginArray();
+    foreach (var value in _rpPlayer.EquippedClothes)
     {
-        private readonly xPlayer player;
-        public PlayerLoadedWriter(xPlayer _player)
-        {
-            this.player = _player;
-        }
-
-        public void OnWrite(IMValueWriter writer)
-        {
-            writer.BeginObject();
-            writer.Name("name");
-            writer.Value(player.name);
-            writer.Name("id");
-            writer.Value(player.id);
-            writer.Name("maxArmor");
-            writer.Value(player.MaxArmor);
-            writer.Name("maxHealth");
-            writer.Value(player.MaxHealth);
-            writer.Name("armor");
-            writer.Value(player.Armor);
-            writer.Name("health");
-            writer.Value(player.Health);
-
-            
-            /* writer.BeginArray();
-            foreach (var value in _rpPlayer.EquippedClothes)
-            {
-                writer.BeginObject();
-                writer.Name("c");
-                writer.Value(value.Value.component);
-                writer.Name("d");
-                writer.Value(value.Value.drawable);
-                writer.Name("t");
-                writer.Value(value.Value.texture);
-                writer.EndObject();
-            }
-            writer.EndArray(); */
-
-            writer.EndObject();
-        }
+        writer.BeginObject();
+        writer.Name("c");
+        writer.Value(value.Value.component);
+        writer.Name("d");
+        writer.Value(value.Value.drawable);
+        writer.Name("t");
+        writer.Value(value.Value.texture);
+        writer.EndObject();
     }
+    writer.EndArray(); */
+
+    writer.EndObject();
+  }
 }
