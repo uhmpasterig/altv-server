9b91e1842c62c9a897eb598e7cd4a9636ccd9550
uhmpasterig <805luca805@gmail.com>
2023-04-14 19:47:12 +0200

    update SammlerModule.cs

--- Modules/Farming/Sammler/SammlerModule.cs
+++ Modules/Farming/Sammler/SammlerModule.cs
@@ -114,6 +114,7 @@ public class SammlerMain : ILoadEvent, IPressedEEvent, IFiveSecondsUpdateEvent
           if (__sammler != null)
           {
             __sammler._propPositions = sammler._propPositions;
+            serverContext.sammler_farming_data.Update(__sammler);
             await serverContext.SaveChangesAsync();
           }
           _logger.Debug("Entity saved");
