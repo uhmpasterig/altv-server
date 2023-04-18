bf1cf5886c6be7eb9a0031f50b70d9b40c1790da
uhmpasterig <805luca805@gmail.com>
2023-04-18 13:28:47 +0200

    update GaragenModule.cs update Garage_Spawns.cs

--- Modules/Garage/GaragenModule.cs
+++ Modules/Garage/GaragenModule.cs
@@ -8,8 +8,16 @@ using _logger = server.Logger.Logger;
 namespace server.Modules.Garage;
 class GaragenModule : ILoadEvent, IPressedEEvent
 {
+  public static List<Models.Garage> garageList = new List<Models.Garage>();
+  
   public async void OnLoad()
   {
+    ServerContext _serverContext = new ServerContext();
+    foreach(Models.Garage garage in _serverContext.Garage.ToList())
+    {
+      garage.ToString();
+      garageList.Add(garage);
+    }
   }
 
   public async Task<bool> OnKeyPressE(xPlayer player)
