af37d48a83a880d200bf1556cc4d644d97f06e68
uhmpasterig <805luca805@gmail.com>
2023-04-20 21:35:11 +0200

    update xMenu.cs

--- Modules/xMenu/xMenu.cs
+++ Modules/xMenu/xMenu.cs
@@ -6,15 +6,43 @@ using AltV.Net.Data;
 using AltV.Net.Async;
 using Newtonsoft.Json;
 using _logger = server.Logger.Logger;
+using server.Handlers.Vehicle;
 
 namespace server.Modules.xMenu;
 
-public class xMenu : IPressedEEvent
+public class xMenu : ILoadEvent
 {
-  public async Task<bool> OnKeyPressE(xPlayer player) 
+  IVehicleHandler _vehicleHandler = new VehicleHandler();
+  private void ToggleLock(xPlayer player)
   {
-    player.SendMessage("Vehicle", "Du hast E gedrückt", 5000, NOTIFYS.INFO);
-    player.SendMessage("Vehicle", "Du hast E gedrückt", 5000, NOTIFYS.ERROR);
-    return false;
+    xVehicle veh = _vehicleHandler.GetClosestVehicle(player.Position, 5);
+    if (veh == null) return;
+    if (!veh.hasControl(player)) return;
+
+    veh.Locked = !veh.Locked;
+    player.SendMessage("Vehicle", $"Das Fahrzeug ist nun {(veh.Locked ? "geschlossen" : "geöffnet")} [{veh.vehicleId}]", 5000, NOTIFYS.INFO);
+  }
+  private void ToggleEngine(xPlayer player) { }
+  private void ToggleTrunk(xPlayer player) { }
+
+  public void OnLoad()
+  {
+    /* player.SendMessage("Vehicle", "Du hast E gedrückt", 5000, NOTIFYS.INFO);
+    player.SendMessage("Vehicle", "Du hast E gedrückt", 5000, NOTIFYS.ERROR); */
+
+    AltAsync.OnClient<xPlayer>("ToggleLock", (player) =>
+    {
+      ToggleLock(player);
+    });
+
+    AltAsync.OnClient<xPlayer>("ToggleEngine", (player) =>
+    {
+      ToggleEngine(player);
+    });
+
+    AltAsync.OnClient<xPlayer>("ToggleTrunk", (player) =>
+    {
+      ToggleTrunk(player);
+    });
   }
 }
