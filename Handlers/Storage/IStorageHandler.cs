d78e89669a3eab4d584727d7119178e7d7559452
uhmpasterig <805luca805@gmail.com>
2023-04-21 17:35:50 +0200

    update IStorageHandler.cs

--- Handlers/Storage/IStorageHandler.cs
+++ Handlers/Storage/IStorageHandler.cs
@@ -10,7 +10,7 @@ public interface IStorageHandler
   Task<xStorage> GetStorage(int storageId);
   Task<bool> LoadStorage(int storageId);
   Task UnloadStorage(int storageId);
-  Task<int> CreateStorage(string name, int slots, float maxWeight, Position position, int ownerId = -1);
+  Task<int> CreateStorage(string name, int slots, float maxWeight, Position? position, int ownerId = -1);
   xStorage GetClosestxStorage(xPlayer player, int range = 2);
   Task CreateAllStorages(xPlayer player);
   Task SaveAllStorages();
