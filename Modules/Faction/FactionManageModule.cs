fcb863c9ac0f79d7cf22bb49b24c0024a15436ef
uhmpasterig <805luca805@gmail.com>
2023-04-27 17:24:54 +0200

    update FactionManageModule.cs and FactionWriter.cs

--- Modules/Faction/FactionManageModule.cs
+++ Modules/Faction/FactionManageModule.cs
@@ -23,16 +23,19 @@ class FactionManageModule : ILoadEvent
   async void SetPlayerFactionDatabase(xPlayer player, int target, List<string> perms, int rank)
   {
     ServerContext _serverContext = new ServerContext();
-    Models.Player? offlinePlayer = await _serverContext.Players.Include(p => p.player_society).FirstAsync(p => p.id == target);
+    Models.Player? offlinePlayer = await _serverContext.Players.Include(p => p.player_society).FirstOrDefaultAsync(p => p.id == target);
     if (offlinePlayer == null) return;
     offlinePlayer.player_society.FactionPerms = perms;
-    offlinePlayer.player_society.faction_rank_id = rank;
+
+    if((offlinePlayer.player_society.faction_rank_id < player.player_society.faction_rank_id) && (rank < player.player_society.faction_rank_id)) {
+      offlinePlayer.player_society.faction_rank_id = rank;
+    };
     await _serverContext.SaveChangesAsync();
   }
 
   public async void OnLoad()
   {
-    AltAsync.OnClient<xPlayer, int, string, int>("faction:leader:setMember", async (player, targetId, _perms, _rank) => {
+    AltAsync.OnClient<xPlayer, int, string, int>("faction:leader:setMember", async (player, targetId, _perms, rank) => {
       if(!(player.player_society.FactionPerms.Contains("faction.leader") || player.player_society.FactionPerms.Contains("faction.management"))) return;
       List<string> perms = JsonConvert.DeserializeObject<List<string>>(_perms)!;
       if(!player.player_society.FactionPerms.Contains("faction.leader") && perms.Contains("faction.management")) return;
@@ -40,10 +43,12 @@ class FactionManageModule : ILoadEvent
       xPlayer? target = await playerHandler.GetPlayer(targetId);  
       if(target != null) {
         target.player_society.FactionPerms = perms;
-        target.player_society.faction_rank_id = _rank;
+        if((target.player_society.faction_rank_id < player.player_society.faction_rank_id) && (rank < player.player_society.faction_rank_id)) {
+          target.player_society.faction_rank_id = rank;
+        };
       };
-      SetPlayerFactionDatabase(player, targetId, perms, _rank);
-      
+      SetPlayerFactionDatabase(player, targetId, perms, rank);
+
     });
   }
 }
