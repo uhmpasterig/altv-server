1ea17bef40ec2b62f465d75bc82c2de314688909
uhmpasterig <805luca805@gmail.com>
2023-04-30 03:43:09 +0200

    update SammlerModule.cs

--- Modules/Farming/SammlerModule.cs
+++ Modules/Farming/SammlerModule.cs
@@ -71,7 +71,7 @@ public class SammlerMain : ILoadEvent, IPressedEEvent, IFiveSecondsUpdateEvent
     // Get the Closest Sammler
     _sammler.ForEach((sammler) =>
     {
-      if (sammler.Position.Distance(player.Position) < 80)
+      if (sammler.Position.Distance(player.Position) < 200)
       {
         _currentSammler = sammler;
       }
