ac205ef5f895b306fdea1c6451fea5a6d2a43115
uhmpasterig <805luca805@gmail.com>
2023-05-01 03:23:51 +0200

    Final

--- Commands/AdminVehicle.cs
+++ Commands/AdminVehicle.cs
@@ -1,4 +1,4 @@
-/* using System;
+using System;
 using AltV.Net.Elements.Entities;
 using AltV.Net.Enums;
 using server.Core;
@@ -7,108 +7,40 @@ using server.Handlers.Vehicle;
 using AltV.Net.Data;
 using AltV.Net;
 using Newtonsoft.Json;
-
+using server.Handlers.Logger;
 using server.Models;
 
 namespace server.Commands;
 
-internal class AdminVehicle : IScript
+public class AdminVehicle : IScript
 {
-  internal static IVehicleHandler _vehicleHandler = new VehicleHandler();
-
-  [Command("car")]
-  public static async void Car(xPlayer player, string model)
-  {
-    Position pos = player.Position;
-    Rotation rot = player.Rotation;
-
-    xVehicle? veh = await _vehicleHandler.CreateVehicle(model, pos, rot);
-    if (veh == null)
-    {
-      player.SendChatMessage("Invalid Model");
-    }
-
-    if (veh.Exists)
-    {
-      player.SetIntoVehicle(veh, 1);
-      player.SendChatMessage("Spawned Vehicle: " + model);
-    }
-  }
-
-  [Command("dv")]
-  public async static void DeleteVehicle(xPlayer player, int range = 5)
-  {
-    Position pos = player.Position;
-    List<xVehicle> vehicles = await _vehicleHandler.GetVehiclesInRadius(pos, range);
-
-    foreach (xVehicle veh in vehicles)
-    {
-      veh.Destroy();
-    }
-  }
-
-  [Command("repair")]
-  public static void RepairVehicle(xPlayer player, int range = 5)
-  {
-    Position pos = player.Position;
-    xVehicle veh = (xVehicle)player.Vehicle;
-
-    if (veh.Exists)
-    {
-      veh.Repair();
-    }
-  }
-
-
-  [Command("fulltune")]
-  public static void FullTune(xPlayer player, int range = 5)
-  {
-    Position pos = player.Position;
-    xVehicle veh = (xVehicle)player.Vehicle;
-
-    player.SendChatMessage("Vehicle Exists: " + veh.Exists);
-    if (veh.Exists)
-    {
-      player.SendChatMessage("Set Fulltune");
-      _vehicleHandler.SetModByType(veh, VehicleModType.Horns, 20);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Spoilers, 2);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Engine, 3);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Transmission, 2);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Suspension, 3);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Turbo, 0);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Brakes, 2);
-      _vehicleHandler.SetModByType(veh, VehicleModType.Plate, 3);
-      _vehicleHandler.SetModByType(veh, VehicleModType.WindowTint, 2);
-    }
-  }
-
   [Command("coords")]
-  public static void GetCoords(xPlayer player, int range = 5)
+  public void GetCoords(xPlayer player, int range = 5)
   {
     string pos = JsonConvert.SerializeObject(player.Position);
     string rot = JsonConvert.SerializeObject(player.Rotation);
 
-    _logger.Info("Position: ");
-    _logger.Log(pos);
-    _logger.Info("Rotation: ");
-    _logger.Log(rot);
-    _logger.Info("Heading: ");
-    _logger.Log(RotationMath.YawToHeading(player.Rotation.Yaw).ToString());
+    Console.WriteLine("Position: ");
+    Console.WriteLine(pos);
+    Console.WriteLine("Rotation: ");
+    Console.WriteLine(rot);
+    Console.WriteLine("Heading: ");
+    Console.WriteLine(RotationMath.YawToHeading(player.Rotation.Yaw).ToString());
 
     if (player.Vehicle != null)
     {
       string vehPos = JsonConvert.SerializeObject(player.Vehicle.Position);
       string vehRot = JsonConvert.SerializeObject(player.Vehicle.Rotation);
 
-      _logger.Info("Vehicle Position: ");
-      _logger.Log(vehPos);
-      _logger.Info("Vehicle Rotation: ");
-      _logger.Log(vehRot);
+      Console.WriteLine("Vehicle Position: ");
+      Console.WriteLine(vehPos);
+      Console.WriteLine("Vehicle Rotation: ");
+      Console.WriteLine(vehRot);
     }
   }
 
   [Command("addshop")]
-  public async static void AddShop(xPlayer player, string name, int type = 1)
+  public async void AddShop(xPlayer player, string name, int type = 1)
   {
     ServerContext _serverContext = new ServerContext();
     Position pos = player.Position;
@@ -126,7 +58,7 @@ internal class AdminVehicle : IScript
   }
 
   [Command("addgarage")]
-  public async static void AddGarage(xPlayer player, string name, int type = 1)
+  public async void AddGarage(xPlayer player, string name, int type = 1)
   {
     ServerContext _serverContext = new ServerContext();
     Position pos = player.Position;
@@ -146,7 +78,7 @@ internal class AdminVehicle : IScript
 
 
   [Command("addgaragespawn")]
-  public async static void AddGarageSpawn(xPlayer player, int garage_id = 1)
+  public async void AddGarageSpawn(xPlayer player, int garage_id = 1)
   {
 
     ServerContext _serverContext = new ServerContext();
@@ -166,16 +98,15 @@ internal class AdminVehicle : IScript
   }
 
   [Command("headoverlay")]
-  public static void HeadOverlay(xPlayer player, int overlayId, int overlayValue, float opacity = 1.0f)
+  public void HeadOverlay(xPlayer player, int overlayId, int overlayValue, float opacity = 1.0f)
   {
     player.SetHeadOverlay((byte)overlayId, (byte)overlayValue, opacity);
   }
 
   [Command("haircolor")]
-  public static void HairColor(xPlayer player, int colorId, int highlightColorId)
+  public void HairColor(xPlayer player, int colorId, int highlightColorId)
   {
     player.HairColor = (byte)colorId;
     player.HairHighlightColor = (byte)highlightColorId;
   }
 }
- */
